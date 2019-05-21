3a1d5ccd930e83b884270a693a2ab2cbc166de65
Marin Garcia <d_maringarcia@tss-hr420-dm22.fanshawec.ca>
2019-05-21 16:46:12 -0400

    experimentation with JS. hover, buttons, titles

--- js/main.js
+++ js/main.js
@@ -10,22 +10,38 @@
 	// and using a CSS selector to find the element we want to use.
 	//
 	// let stores the reference to element as a variable (in memory)
-	let svgGraphic = document.querySelector("#badgeSVG");
-			mainHeadline = document.querySelector(".main-headline");
-			mainSVG = document.querySelector(".svg-raster");
+	//VARIABLES:
+	let svgGraphic = document.querySelector("#badgeSVG"),
+			mainHeadline = document.querySelector(".main-headline"),
+			mainSVG = document.querySelector(".svg-raster"),
+			swapTextButton = document.querySelector(".switch-type");
 
 
-	//functions are reusable pieces of code
+
+	//functions are reusable pieces of code - put these between the variables and the event handling
 	//that you can run any time.
+	//FUNCTIONS:
 	function logMyId(){
 		console.log(this.id);
 
 		this.style.opacity = 0.5;
 	}
 
-	svgGraphic.addEventListener("click", logMyId);
+	function swapText() {
+		mainHeadline.textContent = "Now you're something else!";
+		mainHeadLine.classList.toggle("selected");
 
-	mainHeadline.textContent = "Now you're something else!";
-	mainSVG.textContent = "yadayadayada";
+		subHead.textContent = "yadayadayada";
+		subHead.classList.toggle("selected");
+	}
+
+
+
+//events always go down here
+	svgGraphic.addEventListener("click", logMyId);
+	swapTextButton.addEventListener("click", swapText)
+
+
+
 
 })();
