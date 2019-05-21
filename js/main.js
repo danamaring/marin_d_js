fe6e1798eda02c47c178df180a13b254f7d83fd8
Marin Garcia <d_maringarcia@tss-hr420-dm22.fanshawec.ca>
2019-05-21 16:04:08 -0400

    new updates to JS

--- js/main.js
+++ js/main.js
@@ -4,13 +4,16 @@
 
 
 	// select elements with JavaScript
-	// 
-	// 
+	//
+	//
 	// setting up a variable using the let keyword,
 	// and using a CSS selector to find the element we want to use.
-	// 
+	//
 	// let stores the reference to element as a variable (in memory)
-	let svgGraphic = document.querySelector("#badgeSVG")	
+	let svgGraphic = document.querySelector("#badgeSVG");
+			mainHeadline = document.querySelector(".main-headline");
+			mainSVG = document.querySelector(".svg-raster");
+
 
 	//functions are reusable pieces of code
 	//that you can run any time.
@@ -21,5 +24,8 @@
 	}
 
 	svgGraphic.addEventListener("click", logMyId);
+
+	mainHeadline.textContent = "Now you're something else!";
+	mainSVG.textContent = "yadayadayada";
 
 })();
